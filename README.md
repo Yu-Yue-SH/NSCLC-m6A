f1539db909b6fb31318e025f22c11cc2f191423b
yuyue <yuyue20040413@gmail.com>
2024-04-14 21:35:25 +0800

    add preprocess.R

---
+++ README.md
@@ -0,0 +1,13 @@
+# NSCLC_m6a_project
+
+code for NSCLC_m6a_project
+
+## preprocess.R
+
+1. rename feature names
+2. extract meta data
+3. rerun umap
+
+## m6a_stratify.R
+
+1. 
---
+++ preprocess.R
@@ -0,0 +1,93 @@
+rm(list = ls())
+
+
+# load packages
+library(Seurat)
+
+
+# load data
+NSCLC <- readRDS('data/NSCLC_extended.rds')
+NSCLC <- UpdateSeuratObject(NSCLC)
+
+
+# rename features ####
+counts <- GetAssayData(NSCLC, layer = 'counts')
+data <- GetAssayData(NSCLC, layer = 'data')
+meta_features <- NSCLC[['RNA']]@meta.features
+meta_features[['ENSG_id']] <- rownames(meta_features)
+rownames(meta_features) <- meta_features[['feature_name']]
+rownames(counts) <- meta_features[['feature_name']]
+rownames(data) <- meta_features[['feature_name']]
+meta_data <- NSCLC@meta.data
+re_scANVI <- NSCLC@reductions$scANVI
+re_scVI <- NSCLC@reductions$scVI
+re_umap <- NSCLC@reductions$umap
+
+NSCLC <- CreateSeuratObject(
+  counts = counts,
+  meta.data = meta_data,
+  project = 'NSCLC1m'
+)
+
+NSCLC <- SetAssayData(NSCLC, layer = 'data', data)
+NSCLC[['RNA']]@meta.data <- meta_features
+NSCLC@reductions$scANVI <- re_scANVI
+NSCLC@reductions$scVI <- re_scVI
+NSCLC@reductions$umap <- re_umap
+
+# saveRDS(NSCLC, 'outputs/NSCLC_renamed.rds')
+NSCLC <- readRDS('outputs/NSCLC_renamed.rds')
+
+
+# same process to core data ####
+# load data
+NSCLC <- readRDS('data/NSCLC_core.rds')
+NSCLC <- UpdateSeuratObject(NSCLC)
+
+
+# rename features
+counts <- GetAssayData(NSCLC, layer = 'counts')
+data <- GetAssayData(NSCLC, layer = 'data')
+meta_features <- NSCLC[['RNA']]@meta.features
+meta_features[['ENSG_id']] <- rownames(meta_features)
+rownames(meta_features) <- meta_features[['feature_name']]
+rownames(counts) <- meta_features[['feature_name']]
+rownames(data) <- meta_features[['feature_name']]
+meta_data <- NSCLC@meta.data
+re_scANVI <- NSCLC@reductions$scANVI
+re_scVI <- NSCLC@reductions$scVI
+re_umap <- NSCLC@reductions$umap
+
+NSCLC <- CreateSeuratObject(
+  counts = counts,
+  meta.data = meta_data,
+  project = 'NSCLC1m'
+)
+
+NSCLC <- SetAssayData(NSCLC, layer = 'data', data)
+NSCLC[['RNA']]@meta.data <- meta_features
+NSCLC@reductions$scANVI <- re_scANVI
+NSCLC@reductions$scVI <- re_scVI
+NSCLC@reductions$umap <- re_umap
+
+# saveRDS(NSCLC, file.path('outputs/NSCLC_core_renamed.rds')
+NSCLC <- readRDS('outputs/NSCLC_core_renamed.rds')
+
+
+# extract metadata ####
+meta_data <- NSCLC@meta.data
+
+# saveRDS(meta_data, 'outputs/meta_data.rds')
+meta_data <- readRDS('outputs/meta_data.rds')
+
+
+# rerun umap ####
+NSCLC <- RunUMAP(
+  NSCLC,
+  reduction = 'scANVI',
+  dims = 1:10,
+  reduction.name = 'new_umap'
+)
+
+# saveRDS(NSCLC, 'outputs/NSCLC_new_umap.rds')
+NSCLC <- readRDS('outputs/NSCLC_new_umap.rds')
